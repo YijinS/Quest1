d536a3080dca397ec0fa52895d9321f22824d0ef
소 이 진 <tlatms801@naver.com>
2022-07-03 12:19:37 +0900

    toast 설정

--- app/src/main/java/com/example/quest1/MainActivity.kt
+++ app/src/main/java/com/example/quest1/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.quest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  //res 폴더의 activity_main.xml을 연결해줌. 화면 연결용
+
+        signUpBtn.setOnClickListener {
+            Toast.makeText(this, "회원가입버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+
+        logInBtn.setOnClickListener {
+            Log.d("메인 엑티비티", "로그인 버튼 클릭")
+        }
+
     }
 }
